3476cbe10ea9bc6d9701dca209ef44331c6c88d5
Захаров Алексей <38369192+SimpleWayfarer@users.noreply.github.com>
2025-12-11 14:05:57 +0300

    Update destroy.sh
    Added echo "yes" to skip confirming terraform destroy

--- terraform/destroy.sh
+++ terraform/destroy.sh
@@ -2,4 +2,4 @@
 export YC_TOKEN=$(yc iam create-token)
 export YC_CLOUD_ID=$(yc config get cloud-id)
 export YC_FOLDER_ID=$(yc config get folder-id)
-terraform destroy
+echo "yes" | terraform destroy
