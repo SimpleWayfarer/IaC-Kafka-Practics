043c19c5c911b0b57b1350cbb65085041d0e3221
Захаров Алексей <38369192+SimpleWayfarer@users.noreply.github.com>
2025-12-22 19:28:02 +0300

    Update ansible-init.sh

--- ansible-init.sh
+++ ansible-init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#Получение IP-адрессов
+#Получение IP-адресов
 producer_ip=$(yc compute instance get producer --format json | jq .network_interfaces[0].primary_v4_address.one_to_one_nat.address -r)
 consumer_ip=$(yc compute instance get consumer --format json | jq .network_interfaces[0].primary_v4_address.one_to_one_nat.address -r)
 
-#Запись IP-адрессов
+#Запись IP-адресов
 touch ansible/hosts.ini
 private_key_file="/home/user/.ssh/id_rsa"
 echo "producer ansible_host=$producer_ip ansible_user=ubuntu ansible_private_key_file=$private_key_file ansible_become=yes" >> ansible/hosts.ini
