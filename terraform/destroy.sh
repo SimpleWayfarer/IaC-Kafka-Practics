50ef85be487481e237c1b338ae5b148e4e874b71
Захаров Алексей <38369192+SimpleWayfarer@users.noreply.github.com>
2025-12-23 15:36:32 +0300

    Update destroy.sh

--- terraform/destroy.sh
+++ terraform/destroy.sh
@@ -3,4 +3,4 @@ export YC_TOKEN=$(yc iam create-token)
 export YC_CLOUD_ID=$(yc config get cloud-id)
 export YC_FOLDER_ID=$(yc config get folder-id)
 terraform init
-echo "yes" | terraform destroy
+terraform destroy -auto-approve
